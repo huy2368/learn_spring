5fa8deff3b89ef0b3be9230f2b3aa1cb8ff7e2cc
Nguyen The Huy <huynt@mpos.vn>
2021-09-13 21:53:32 +0700

    greeting: request parameter, json response. print all beans

---
+++ greeting/src/test/java/com/huy2368/greeting/GreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huy2368.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
